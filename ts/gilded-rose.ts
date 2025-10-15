3f92392ae66f5e84c84bda4b1579ba749445510a
Anubhav Saini <write.to.anubhav.saini@gmail.com>
2025-10-15 07:22:35 +0000

    refactoring: updated function updateIfExpired and cleanup
    1. First we broke the nested if conditions in updateIfExpired function.
    2. Then, we used switch to clean up code
    3. Updated backstage pass quality on expiry directly to 0
    
    With this our efforts to cleanup this code is complete.

--- ts/gilded-rose.ts
+++ ts/gilded-rose.ts
@@ -19,23 +19,11 @@ export class GildedRose {
     }
 
     updateIfExpired(i: number) {
-        if (this.items[i].name != "Aged Brie") {
-            if (
-                this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-            ) {
-                if (this.items[i].quality > 0) {
-                    if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        this.items[i].quality = this.items[i].quality - 1;
-                    }
-                }
-            } else {
-                this.items[i].quality = this.items[i].quality -
-                    this.items[i].quality;
-            }
-        } else {
-            if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-            }
+        switch (this.items[i].name) {
+            case "Sulfuras, Hand of Ragnaros": { } break;
+            case "Aged Brie": { this.items[i].quality = Math.min(50, this.items[i].quality + 1); } break;
+            case "Backstage passes to a TAFKAL80ETC concert": { this.items[i].quality = 0; } break;
+            default: { this.items[i].quality = Math.max(0, this.items[i].quality - 1); } break;
         }
     }
 
