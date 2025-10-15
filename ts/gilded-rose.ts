e7cf66490b7dd3b25bbe69cdcdf99e0c8b82b047
Anubhav Saini <write.to.anubhav.saini@gmail.com>
2025-10-15 06:48:22 +0000

    refactoring: moved functionality to different function

--- ts/gilded-rose.ts
+++ ts/gilded-rose.ts
@@ -18,6 +18,27 @@ export class GildedRose {
         this.items = items;
     }
 
+    updateIfExpired(i: number) {
+        if (this.items[i].name != "Aged Brie") {
+            if (
+                this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
+            ) {
+                if (this.items[i].quality > 0) {
+                    if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+                        this.items[i].quality = this.items[i].quality - 1;
+                    }
+                }
+            } else {
+                this.items[i].quality = this.items[i].quality -
+                    this.items[i].quality;
+            }
+        } else {
+            if (this.items[i].quality < 50) {
+                this.items[i].quality = this.items[i].quality + 1;
+            }
+        }
+    }
+
     updateQuality() {
         for (let i = 0; i < this.items.length; i++) {
             switch (this.items[i].name) {
@@ -25,19 +46,9 @@ export class GildedRose {
                 case "Backstage passes to a TAFKAL80ETC concert": { } break;
                 case "Sulfuras, Hand of Ragnaros": { } break;
                 default: {
-                        this.items[i].quality = Math.max(0, this.items[i].quality - 1);
-                        break;
+                    this.items[i].quality = Math.max(0, this.items[i].quality - 1);
+                    break;
                 }
-            }
-
-            // if-block
-            if (
-                this.items[i].name != "Aged Brie" &&
-                this.items[i].name != "Backstage passes to a TAFKAL80ETC concert" &&
-                this.items[i].name != "Sulfuras, Hand of Ragnaros" &&
-                this.items[i].quality > 0
-            ) {
-                // this.items[i].quality = this.items[i].quality - 1;
             }
 
             // created new block...
@@ -64,25 +75,9 @@ export class GildedRose {
             if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
                 this.items[i].sellIn = this.items[i].sellIn - 1;
             }
+            
             if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != "Aged Brie") {
-                    if (
-                        this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-                    ) {
-                        if (this.items[i].quality > 0) {
-                            if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                this.items[i].quality = this.items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        this.items[i].quality = this.items[i].quality -
-                            this.items[i].quality;
-                    }
-                } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1;
-                    }
-                }
+                this.updateIfExpired(i);
             }
         }
 
